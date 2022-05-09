0b59f999e3a5eb0d31876bc4d4dbbaca2cfd5e1e
Teng <luoteng91@gmail.com>
2022-05-09 04:02:01 -0400

    Add files via upload

---
+++ site/module10-solution/src/public/myinfo/myinfo.controller.js
@@ -0,0 +1,14 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('MyInfoController', MyInfoController);
+
+MyInfoController.$inject = ['info'];
+function MyInfoController(info) {
+  var $ctrl = this;
+  $ctrl.info = info;
+}
+
+
+})();
