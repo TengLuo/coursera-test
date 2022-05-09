56405c3a0b2074cbe5101d379a058ce7184cb8c5
Teng <luoteng91@gmail.com>
2022-05-09 03:51:56 -0400

    Add files via upload

---
+++ site/module10-solution/src/common/myInfo.service.js
@@ -0,0 +1,39 @@
+(function () {
+"use strict";
+
+angular.module('common')
+.service('MyInfoService', MyInfoService);
+
+
+MyInfoService.$inject = ['$http', 'ApiPath'];
+function MyInfoService($http, ApiPath) {
+  var service = this;
+
+  service.getCategories = function () {
+    return $http.get(ApiPath + '/categories.json').then(function (response) {
+      return response.data;
+    });
+  };
+
+  service.getFavorite = function (short_name) {
+    return $http.get(ApiPath + '/menu_items/' + short_name +'.json').then(function (response) {
+      return response.data;
+    });
+  };
+
+  service.getMenuItems = function (category) {
+    var config = {};
+    if (category) {
+      config.params = {'category': category};
+    }
+
+    return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
+      return response.data;
+    });
+  };
+
+}
+
+
+
+})();
