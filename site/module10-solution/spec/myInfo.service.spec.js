5398389bed1e293cbe5c89d8d99f43e7cb275118
Teng <luoteng91@gmail.com>
2022-05-10 20:57:52 -0400

    Update myInfo.service.spec.js

--- site/module10-solution/spec/myInfo.service.spec.js
+++ site/module10-solution/spec/myInfo.service.spec.js
@@ -10,16 +10,17 @@ describe('MyInfoService get valid item check', function () {
     inject(function ($injector) {
       myInfoService = $injector.get('MyInfoService');
       $httpBackend = $injector.get('$httpBackend');
-      ApiBasePath = $injector.get('ApiBasePath');
+      ApiBasePath = $injector.get('ApiPath');
     });
   });
 
   it('should return L1 item object', function() {
-    $httpBackend.whenGET(ApiBasePath + 'menu_items/L1.json').respond({short_name: "L1"});
-    myInfoService.getFavorite('L1').then(function(response) {
-      expect(response).toEqual({short_name: "L1"});
+    var short_name = "L1";
+    $httpBackend.whenGET(ApiBasePath + '/menu_items/' + short_name + '.json')
+    .respond({"id":193,"short_name":"L1","name":"Orange Chicken","description":"chunks of chicken, breaded and deep-fried with sauce containing orange peels; white meat by request: for pint $1 extra, for large $2 extra","price_small":null,"price_large":9.75,"small_portion_name":null,"large_portion_name":null,"created_at":"2022-05-08T08:27:47.790Z","updated_at":"2022-05-08T08:27:47.790Z","category_short_name":"L","image_present":true});
+    myInfoService.getFavorite(short_name).then(function(response) {
+      expect(response).toEqual({"id":193,"short_name":"L1","name":"Orange Chicken","description":"chunks of chicken, breaded and deep-fried with sauce containing orange peels; white meat by request: for pint $1 extra, for large $2 extra","price_small":null,"price_large":9.75,"small_portion_name":null,"large_portion_name":null,"created_at":"2022-05-08T08:27:47.790Z","updated_at":"2022-05-08T08:27:47.790Z","category_short_name":"L","image_present":true});
     });
     $httpBackend.flush();
   });
-
 });
